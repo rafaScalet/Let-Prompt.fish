da5952feb627e284f6a811367bc668628776673f
Rafael Scalet <114953004+rafaScalet@users.noreply.github.com>
2025-02-12 21:55:22 -0300

    Initial commit

---
+++ conf.d/let-prompt.fish
@@ -0,0 +1,21 @@
+if status is-interactive
+  function fish_greeting
+    if type -q fastfetch
+      fastfetch --config ~/.config/fastfetch/greeting.jsonc
+      if test $status -ne 0
+        fastfetch
+      end
+    else
+      echo "Bem vindo "(set_color magenta)(whoami)(set_color normal)", ao seu novo setup"
+      echo "instale o "(set_color cyan)"fastfetch "(set_color normal)"para começar"
+    end
+  end
+
+
+  function fish_title
+    set -l current_dir (basename (pwd))
+    set -l parent_dir (basename (dirname (pwd)))
+
+    echo $parent_dir/$current_dir
+  end
+end
