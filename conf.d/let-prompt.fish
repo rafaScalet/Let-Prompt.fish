0f25a0ee4b335bbcaf1737b41b3a04457e27492a
Rafael Scalet <114953004+rafaScalet@users.noreply.github.com>
2025-02-14 11:13:18 -0300

    Update let-prompt.fish

--- conf.d/let-prompt.fish
+++ conf.d/let-prompt.fish
@@ -1,7 +1,7 @@
 if status is-interactive
   function fish_greeting
     if type -q fastfetch
-      fastfetch --config ~/.config/fastfetch/greeting.jsonc
+      fastfetch --config ~/.config/fastfetch/greeting.jsonc 2> /dev/null
     else
       echo "Welcome "(set_color cyan)(whoami)(set_color normal)", to your new setup of "(set_color yellow)(hostnamectl | grep "Operating System" | cut -d ' ' -f3-)(set_color normal)
       echo "install "(set_color green)"fastfetch "(set_color normal)"and "(set_color magenta)"starship "(set_color normal)"to start"
