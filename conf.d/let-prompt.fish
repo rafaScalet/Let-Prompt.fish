e37d3c14fb29859a3505fc80b139a0e54f556c6f
rafaScalet <114953004+rafaScalet@users.noreply.github.com>
2025-02-14 09:39:17 -0300

    add a default prompt

--- conf.d/let-prompt.fish
+++ conf.d/let-prompt.fish
@@ -1,22 +1,35 @@
 if status is-interactive
   function fish_greeting
-    if type -q fastfetch
+    if type -q fastfetch --config ~/.config/fastfetch/greeting.jsonc
       fastfetch --config ~/.config/fastfetch/greeting.jsonc
-      if test $status -ne 0
-        fastfetch
-      end
     else
       echo "Welcome "(set_color cyan)(whoami)(set_color normal)", to your new setup of "(set_color yellow)(hostnamectl | grep "Operating System" | cut -d ' ' -f3-)(set_color normal)
       echo "install "(set_color green)"fastfetch "(set_color normal)"and "(set_color magenta)"starship "(set_color normal)"to start"
     end
   end
 
-
   function fish_title
     set -l current_dir (basename (pwd))
     set -l parent_dir (basename (dirname (pwd)))
 
     echo $parent_dir/$current_dir
+  end
+
+  function fish_prompt
+    set_color magenta
+    printf '%s' $USER
+    set_color normal
+    printf ' in '
+
+    set_color yellow
+    printf '%s' (basename (pwd))
+    set_color normal
+
+    # Line 2
+    echo
+    set_color green
+    printf '↪ '
+    set_color normal
   end
 
   if type -q starship
