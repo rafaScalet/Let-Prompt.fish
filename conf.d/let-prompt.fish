be614a62e366800f6cac2015a9b47419c0089908
rafaScalet <114953004+rafaScalet@users.noreply.github.com>
2025-02-23 22:45:09 -0300

    change title to match the directory

--- conf.d/let-prompt.fish
+++ conf.d/let-prompt.fish
@@ -12,7 +12,36 @@ if status is-interactive
     set -l current_dir (basename (pwd))
     set -l parent_dir (basename (dirname (pwd)))
 
-    echo $parent_dir/$current_dir
+    switch $current_dir
+      case (whoami)
+        echo 
+      case 'Documentos' 'Documents'
+        echo 󰈙
+      case 'Models' 'Modelos'
+        echo 󰛼
+      case 'Downloads'
+        echo 
+      case 'Music' 'Músicas'
+        echo 󰝚
+      case 'Pictures' 'Imagens'
+        echo 
+      case 'Projects' 'Developer' 'Dev'
+        echo 󰲋
+      case 'Games'
+        echo 󰮂
+      case 'Public' 'Público'
+        echo 󰖟
+      case 'Vídeos' 'Videos'
+        echo 
+      case 'Desktop' 'Área de trabalho'
+        echo 
+      case 'Dotfiles' 'Config' 'Configurations'
+        echo 
+      case 'Annotations' 'Notes'
+        echo 
+      case '*'
+        echo $parent_dir/$current_dir
+    end
   end
 
   function fish_prompt
